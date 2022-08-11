63acbd4a4aa5b9489372aeaba4ff70e380ace0a1
Richard mendoza ORDENADOR-PC <rmendoza118@gmail.com>
2022-08-11 18:09:56 -0400

    inicializado suma 2 numeros

---
+++ src/main/java/com/mycompany/test1/Test1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.test1;
+import java.util.Scanner; 
+/**
+ *
+ * @author unkont
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+         int numero1;
+        int numero2;
+        
+        System.out.println("Ingresa el primero número");
+        Scanner num1 = new Scanner (System.in);
+        numero1 = num1.nextInt();
+        System.out.println("Ingresa el segundo número");
+        Scanner num2 = new Scanner(System.in);
+        numero2 = num2.nextInt();
+        int suma = numero1 + numero2;
+        System.out.println("La suma de los dos números es: " + suma);
+    }
+}
